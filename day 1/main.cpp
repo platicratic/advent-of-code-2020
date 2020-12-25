66980e3651d0342104333ecde65f8768c1ac3dc7
Cristian <cqer1aud@gmail.com>
2020-12-25 18:39:59 +0200

    day 1

---
+++ day 1/main.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+ifstream fin("input.in");
+ofstream fout("output.out");
+
+vector<int> V;
+
+void read() {
+    int number;
+    while (fin >> number) {
+        V.push_back(number);
+    }
+}
+
+// Part Two
+void solve() {
+    for (int i = 0; i < V.size(); i ++) {
+        for (int j = i + 1; j < V.size(); j ++) {
+            for (int k = j + 1; k < V.size(); k ++) {
+                if (V[i] + V[j] + V[k] == 2020) {
+                    fout << V[i] * V[j] * V[k];
+                    return;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    read();
+    solve();
+    return 0;
+}
